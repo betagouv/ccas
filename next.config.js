6d9940b2fb2e312d470c06ebec7c445a14755fb5
Thomas Guillet <guillet.thomas@gmail.com>
2019-05-14 08:02:15 +0200

    Première publication sur GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,2 +1,11 @@
+const isProduction = 'production' === process.env.NODE_ENV
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS()
+
+module.exports = withCSS({
+  assetPrefix: isProduction ? '/ccas' : '',
+  exportPathMap: function(defaultPathMap) {
+    return {
+      '/': { page: '/' }
+    }
+  }
+})
